b41beeef59b83bbc8749aa0848772b687c14f41f
gaddafi <gaddafiadamu400@gmail.com>
2022-11-13 01:16:38 +0100

    static modifocation

--- library/static/library/index.js
+++ library/static/library/index.js
@@ -1,18 +1,32 @@
-// setup nav
-const navBtn = document.getElementById("nav-btn");
-const navbar = document.getElementById("navbar");
-const navClose = document.getElementById("nav-close");
+// // setup nav
+// const navBtn = document.getElementById("nav-btn");
+// const navbar = document.getElementById("navbar");
+// const navClose = document.getElementById("nav-close");
 
-// show nav
-navBtn.addEventListener("click", () => {
-  navbar.classList.add("showNav");
-});
-// close nav
-navClose.addEventListener("click", () => {
-  navbar.classList.remove("showNav");
-});
-// setup date
-const date = (document.getElementById("date").innerHTML =
-  new Date().getFullYear());
+// // show nav
+// navBtn.addEventListener("click", () => {
+//   navbar.classList.add("showNav");
+// });
+// // close nav
+// navClose.addEventListener("click", () => {
+//   navbar.classList.remove("showNav");
+// });
+// // setup date
 
 // clearInput
+const getElement = (selector) => {
+  const el = document.querySelector(selector);
+  if (el) return el;
+  throw new Error(`Please check your classes : ${selector} does not exist`);
+};
+
+const navToggle = getElement(".nav-toggle");
+const links = getElement(".links");
+
+navToggle.addEventListener("click", function () {
+  links.classList.toggle("show-links");
+});
+
+
+const date = (document.getElementById("date").innerHTML =
+   new Date().getFullYear());
